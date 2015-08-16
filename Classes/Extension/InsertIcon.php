503dc273eabd558c6aaff65f458c7cabf1687881
Pascal Mayer <laxap4@gmail.com>
2015-08-16 02:51:17 +0200

    Supporting TYPO3 7

--- Classes/Extension/InsertIcon.php
+++ Classes/Extension/InsertIcon.php
@@ -23,11 +23,15 @@ namespace Laxap\Iconfont\Extension;
  *
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
+
+use TYPO3\CMS\Rtehtmlarea\RteHtmlAreaApi;
+use TYPO3\CMS\Core\Utility\VersionNumberUtility;
+
 /**
  * Insert Icon plugin for htmlArea RTE
  *
  */
-class InsertIcon extends \TYPO3\CMS\Rtehtmlarea\RteHtmlAreaApi {
+class InsertIcon extends RteHtmlAreaApi {
 
 	// The key of the extension that is extending htmlArea RTE
 	protected $extensionKey = 'iconfont';
@@ -35,29 +39,25 @@ class InsertIcon extends \TYPO3\CMS\Rtehtmlarea\RteHtmlAreaApi {
 	// The name of the plugin registered by the extension
 	protected $pluginName = 'InsertIcon';
 
-	// Path to this main locallang file of the extension relative to the extension dir.
-	protected $relativePathToLocallangFile = '';
-
-	// Path to the skin (css) file relative to the extension dir.
-	protected $relativePathToSkin = 'extensions/InsertIcon/skin/htmlarea.css';
-
-	// Reference to the invoking object
-	protected $htmlAreaRTE;
-
-	// Reference to RTE PageTSConfig
-	protected $thisConfig;
-
-	// Reference to RTE toolbar array
-	protected $toolbar;
-
-	// Frontend language array
-	protected $LOCAL_LANG;
-
 	protected $pluginButtons = 'fonticon';
 
 	protected $convertToolbarForHtmlAreaArray = array(
 		'fonticon' => 'InsertIcon'
 	);
+
+	// --- T3 6.x ---
+	// Path to this main locallang file of the extension relative to the extension dir.
+	protected $relativePathToLocallangFile = '';
+	// Path to the skin (css) file relative to the extension dir.
+	protected $relativePathToSkin = 'extensions/InsertIcon/skin/htmlarea.css';
+	// Reference to the invoking object
+	protected $htmlAreaRTE;
+	// Reference to RTE PageTSConfig
+	protected $thisConfig;
+	// Reference to RTE toolbar array
+	protected $toolbar;
+	// Frontend language array
+	protected $LOCAL_LANG;
 
 	/**
 	 * Return JS configuration of the htmlArea plugins registered by the extension
@@ -65,10 +65,11 @@ class InsertIcon extends \TYPO3\CMS\Rtehtmlarea\RteHtmlAreaApi {
 	 * @param  integer	$RTEcounter		Relative id of the RTE editing area in the form
 	 * @return string					JS configuration for registered plugins
 	 */
-	public function buildJavascriptConfiguration($RTEcounter) {
-		//global $TSFE, $LANG;
-		$registerRTEinJavascriptString = '';
-		return $registerRTEinJavascriptString;
+	public function buildJavascriptConfiguration($RTEcounter = 0) {
+		if ( VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version) >= 7000000 ) {
+			return parent::buildJavascriptConfiguration($RTEcounter);
+		}
+		return '';
 	}
 
 }
