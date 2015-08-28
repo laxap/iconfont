5ce3071112dd4c1c76d8e6f5504eba0b53a4f159
Pascal Mayer <laxap4@gmail.com>
2015-08-28 23:40:04 +0200

    Composer file added.

--- Classes/Extension/InsertIcon.php
+++ Classes/Extension/InsertIcon.php
@@ -5,12 +5,13 @@ namespace Laxap\Iconfont\Extension;
  *  Copyright notice
  *
  *  (c) 2015 Pascal Mayer <typo3(a)bsdist.ch>
+ *
  *  All rights reserved
  *
- *  This script is part of the Typo3 project. The Typo3 project is
+ *  This script is part of the TYPO3 project. The TYPO3 project is
  *  free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
+ *  the Free Software Foundation; either version 3 of the License, or
  *  (at your option) any later version.
  *
  *  The GNU General Public License can be found at
