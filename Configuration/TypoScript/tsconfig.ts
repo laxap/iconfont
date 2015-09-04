35eb15b85846bc90d56bc976d5b97fe2fd042c4b
Pascal Mayer <laxap4@gmail.com>
2015-09-04 23:20:50 +0200

    CSS for RTE changed  (RTE.default.contentCSS)

--- Configuration/TypoScript/tsconfig.ts
+++ Configuration/TypoScript/tsconfig.ts
@@ -3,6 +3,8 @@
  * RTE.default
  */
 RTE.default {
+	# Change CSS for RTE (with font-awesome.min.css included)
+	contentCSS = typo3conf/ext/iconfont/Resources/Public/Css/rte.css
 	# Tags allowed outside p & div
 	proc.allowTagsOutside := addToList(i)
 	# Do not remove i tags
@@ -11,25 +13,25 @@ RTE.default {
 	showButtons := addToList(fonticon)
 	# Add at end (without the toolbarOrder the fonticon button would be on a single row on top of all other)
 	toolbarOrder (
-	bar, blockstylelabel, blockstyle, textstylelabel, textstyle, linebreak,
-			bar, formattext, bold,  strong, italic, emphasis, big, small, insertedtext, deletedtext, citation, code,
-			definition, keyboard, monospaced, quotation, sample, variable, bidioverride, strikethrough, subscript, superscript, underline, span,
-			bar, fontstyle, fontsize, bar, formatblock, insertparagraphbefore, insertparagraphafter, blockquote, line,
-			bar, left, center, right, justifyfull,
-			bar, orderedlist, unorderedlist, definitionlist, definitionitem, outdent, indent,
-			bar, language, showlanguagemarks,lefttoright, righttoleft,
-			bar, textcolor, bgcolor, textindicator,
-			bar, editelement, showmicrodata,
-			bar, image, emoticon, insertcharacter, insertsofthyphen, abbreviation, user,
-			bar, link, unlink,
-			bar, table,
-			bar, findreplace, spellcheck,
-			bar, chMode, inserttag, removeformat, bar, copy, cut, paste, pastetoggle, pastebehaviour, bar, undo, redo, bar, about, linebreak,
-			bar, toggleborders,
-			bar, tableproperties, tablerestyle, bar, rowproperties, rowinsertabove, rowinsertunder, rowdelete, rowsplit, bar,
-			columnproperties, columninsertbefore, columninsertafter, columndelete, columnsplit, bar,
-			cellproperties, cellinsertbefore, cellinsertafter, celldelete, cellsplit, cellmerge,
-			bar, fonticon
+		bar, blockstylelabel, blockstyle, textstylelabel, textstyle, linebreak,
+		bar, formattext, bold,  strong, italic, emphasis, big, small, insertedtext, deletedtext, citation, code,
+		definition, keyboard, monospaced, quotation, sample, variable, bidioverride, strikethrough, subscript, superscript, underline, span,
+		bar, fontstyle, fontsize, bar, formatblock, insertparagraphbefore, insertparagraphafter, blockquote, line,
+		bar, left, center, right, justifyfull,
+		bar, orderedlist, unorderedlist, definitionlist, definitionitem, outdent, indent,
+		bar, language, showlanguagemarks,lefttoright, righttoleft,
+		bar, textcolor, bgcolor, textindicator,
+		bar, editelement, showmicrodata,
+		bar, image, emoticon, insertcharacter, insertsofthyphen, abbreviation, user,
+		bar, link, unlink,
+		bar, table,
+		bar, findreplace, spellcheck,
+		bar, chMode, inserttag, removeformat, bar, copy, cut, paste, pastetoggle, pastebehaviour, bar, undo, redo, bar, about, linebreak,
+		bar, toggleborders,
+		bar, tableproperties, tablerestyle, bar, rowproperties, rowinsertabove, rowinsertunder, rowdelete, rowsplit, bar,
+		columnproperties, columninsertbefore, columninsertafter, columndelete, columnsplit, bar,
+		cellproperties, cellinsertbefore, cellinsertafter, celldelete, cellsplit, cellmerge,
+		bar, fonticon
 	)
 
 }
