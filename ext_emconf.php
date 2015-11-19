e7dfac3a7e31bd0c995dddd3299d939b2784a906
Pascal Mayer <laxap4@gmail.com>
2015-11-19 22:15:46 +0100

    0.7.2-dev

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_email' => 'typo3@bsdist.ch',
 	'author_company' => '',
 	'shy' => '',
-	'version' => '0.7.1',
+	'version' => '0.7.2-dev',
 	'priority' => '',
 	'module' => '',
 	'state' => 'beta',
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'lockType' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-7.4.99',
+			'typo3' => '6.2.0-7.6.99',
 		),
 		'conflicts' => array(
 			'fontawesome' => '',
