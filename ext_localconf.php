ad0a3e309a3fc0a4ac330e057faed8cad0776707
Pascal Mayer <laxap4@gmail.com>
2015-12-30 22:22:49 +0100

    Loading of TS constants via addTypoScriptConstants() moved to ext_tables.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,10 @@
 <?php
 if (!defined ('TYPO3_MODE')) die ('Access denied.');
 
+$iconFont = 'fontawesome';
+$customIconDefFile = '';
+$iconFontOption = array(array('', 0));
+
 // --- Get extension configuration ---
 $extConf = array();
 if ( strlen($_EXTCONF) ) {
@@ -27,6 +31,50 @@ if ( $extConf['iconFont'] != 'custom' ) {
 	}
 	$TYPO3_CONF_VARS['EXTCONF']['rtehtmlarea']['plugins'][$pluginName]['addIconsToSkin'] = 1;
 	$TYPO3_CONF_VARS['EXTCONF']['rtehtmlarea']['plugins'][$pluginName]['disableInFE'] = 0;
+}
+
+// --- Typoscript Constants and page TSconfig  --
+//
+switch ( $extConf['iconFont'] ) {
+	case 'fontello':
+		$fontCssFile = 'typo3conf/ext/iconfont/Resources/Public/Lib/fontello/css/fontello.min.css';
+		$contentCssFile = 'typo3conf/ext/iconfont/Resources/Public/Css/rte/fontello.css';
+		$fontClassPrefix = 'icon-';
+		$fontClassAddon = '';
+		break;
+
+	case 'custom':
+		$fontCssFile = '';
+		$contentCssFile = '';
+		$fontClassPrefix = '';
+		$fontClassAddon = '';
+		break;
+
+	default:
+		$fontCssFile = 'typo3conf/ext/iconfont/Resources/Public/Lib/font-awesome/css/font-awesome.min.css';
+		$contentCssFile = 'typo3conf/ext/iconfont/Resources/Public/Css/rte/font-awesome.css';
+		$fontClassPrefix = 'fa fa-';
+		$fontClassAddon = '';
+		break;
+}
+
+// TypoScript constants
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptConstants('
+plugin.tx_iconfont {
+    # cat=tx_iconfont/base/010; type=string; label=Path to icon font css file
+    cssFile = ' . $fontCssFile . '
+    # cat=tx_iconfont/base/011; type=string; label=Icon font class prefix
+    fontClassPrefix = ' . $fontClassPrefix . '
+    # cat=tx_iconfont/base/012; type=string; label=Icon font class addon
+    fontClassAddon = ' . $fontClassAddon . '
+}');
+
+// Default TS for iconfont
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Icon Font');
+
+// Optional for modified header (option in bootstrap_core to have subheader in header tag)
+if ( isset($extConf['enableHeaderRenderingOption']) && $extConf['enableHeaderRenderingOption'] ) {
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/Header', 'Subheader in header (addon)');
 }
 
 // --- Load default page TSconfig ---
--- ext_tables.php
+++ ext_tables.php
@@ -63,41 +63,6 @@ $tempColumn['tx_iconfont_icon']['config']['suppress_icons'] = 1;
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addFieldsToPalette('tt_content', 'headers', 'tx_iconfont_icon', 'after:header_layout');
 
 
-// --- Typoscript Constants and page TSconfig  --
-//
-switch ( $iconFont ) {
-	case 'fontello':
-		$fontCssFile = 'typo3conf/ext/iconfont/Resources/Public/Lib/fontello/css/fontello.min.css';
-		$contentCssFile = 'typo3conf/ext/iconfont/Resources/Public/Css/rte/fontello.css';
-		$fontClassPrefix = 'icon-';
-		$fontClassAddon = '';
-		break;
-
-	case 'custom':
-		$fontCssFile = '';
-		$contentCssFile = '';
-		$fontClassPrefix = '';
-		$fontClassAddon = '';
-		break;
-
-	default:
-		$fontCssFile = 'typo3conf/ext/iconfont/Resources/Public/Lib/font-awesome/css/font-awesome.min.css';
-		$contentCssFile = 'typo3conf/ext/iconfont/Resources/Public/Css/rte/font-awesome.css';
-		$fontClassPrefix = 'fa fa-';
-		$fontClassAddon = '';
-		break;
-}
-
-// TypoScript constants
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptConstants('
-plugin.tx_iconfont {
-    # cat=tx_iconfont/base/010; type=string; label=Path to icon font css file
-    cssFile = ' . $fontCssFile . '
-    # cat=tx_iconfont/base/011; type=string; label=Icon font class prefix
-    fontClassPrefix = ' . $fontClassPrefix . '
-    # cat=tx_iconfont/base/012; type=string; label=Icon font class addon
-    fontClassAddon = ' . $fontClassAddon . '
-}');
 
 // Page TSconfig
 if ( $iconFont != 'custom' ) {
@@ -115,14 +80,6 @@ RTE.default.contentCSS = ' . $contentCssFile . '
 ');
 	}
 
-}
-
-// Default TS for iconfont
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Icon Font');
-
-// Optional for modified header (option in bootstrap_core to have subheader in header tag)
-if ( isset($extConf['enableHeaderRenderingOption']) && $extConf['enableHeaderRenderingOption'] ) {
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/Header', 'Subheader in header (addon)');
 }
 
 // for 7.4+
